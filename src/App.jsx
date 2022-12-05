79b514739b21fd230c5d47ab08d7e4ee90d2613f
Abdihafid <abdihafid123@hotmail.co.uk>
2022-12-05 13:37:31 +0000

    added axios and made framework of layout

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+function App() {
+
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=birmingham,uk&appid=4f318c295b3aa42d94315eacbbf5ade9'
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Birmingham</p>
+          </div>
+          <div className="temp">
+            <h1>60&deg;F</h1>
+          </div>
+          <div className="description">
+            <p>clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65&deg;F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
